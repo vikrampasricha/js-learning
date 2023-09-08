d7728e349f72029aa635dfa718254cde6726c776
vikrampasricha <76536081+vikrampasricha@users.noreply.github.com>
2023-09-08 07:40:06 +0000

    datatypes

---
+++ practice/datatypes.js
@@ -0,0 +1,13 @@
+// treats all JS code as newer version
+"use strict";
+//we are using nodejs,not browser
+//alert(3+3)
+
+let name="abcd"
+let age=18
+let isLoggedIn=false;
+// null is standalone value example of temp
+let state=null
+
+console.log(typeof (undefined))// undefined
+console.log(typeof(null))//object
