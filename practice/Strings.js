e74b03561b2d9d9878bea13c234bd5653a2b126e
vikrampasricha <76536081+vikrampasricha@users.noreply.github.com>
2023-09-13 15:46:21 +0000

    string

---
+++ practice/Strings.js
@@ -0,0 +1,11 @@
+console.log("abcd");
+
+const first="Vikram"
+const second="Pasricha"
+//old way of concating two strings
+console.log(first+second);
+
+//modern way using   `(back tick) and ${}
+console.log(`my first naeme is ${first}and last name is${second}`);
+//another way of creating string using object
+const gname=new String('abcde')
